a46230f4726ed2f25f9807ece31ff6ac29ecf66c
Douglas Ferreira <douglas.ferreira@ifood.com.br>
2017-01-19 13:34:36 -0200

    Updated multi protocols syntax on Router

--- lib/templates/medusa/swift/Router/VIPERRouter.swift
+++ lib/templates/medusa/swift/Router/VIPERRouter.swift
@@ -13,7 +13,7 @@ final class VIPERRouter: VIPERRouterProtocol {
 
         // Generating module components
         let view: VIPERViewProtocol = VIPERView()
-        let presenter: protocol<VIPERPresenterProtocol, VIPERInteractorOutputProtocol> = VIPERPresenter()
+        let presenter: VIPERPresenterProtocol & VIPERInteractorOutputProtocol = VIPERPresenter()
         let interactor: VIPERInteractorInputProtocol = VIPERInteractor()
         let APIDataManager: VIPERAPIDataManagerInputProtocol = VIPERAPIDataManager()
         let localDataManager: VIPERLocalDataManagerInputProtocol = VIPERLocalDataManager()
